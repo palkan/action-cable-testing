7db854fad0743efe6dc7434f5d58fde6c1693e4c
Paul Carey <paulccarey@gmail.com>
2018-11-13 15:22:47 +0000

    add system tests to feature context include

--- lib/action_cable/testing/rspec/features.rb
+++ lib/action_cable/testing/rspec/features.rb
@@ -2,5 +2,5 @@
 
 # Use async adapter for features specs by deafault
 RSpec.configure do |config|
-  config.include_context "action_cable:async", type: :feature
+  config.include_context "action_cable:async", type: [:feature, :system]
 end
